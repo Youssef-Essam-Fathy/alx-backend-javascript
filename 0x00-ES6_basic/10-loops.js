6580d6e69f911453105395105637fdb20affaa85
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-05-28 02:29:53 +0300

    alx-backend-javascript

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   let list = [];
   for (let value of array) {
     list.push(appendString + value);
-  }
+}
 
   return list;
 }
