a48b39b18b3cea728f64d5d9099f7bd942c62afa
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-05-27 21:19:50 +0300

    alx-backend-javascript

--- 0x00-ES6_basic/test.js
+++ 0x00-ES6_basic/test.js
@@ -1,13 +1,23 @@
-class Person {
-  name;
+// class Person {
+//   name;
 
-  constructor (name) {
-    this.name = name;
-  }
+//   constructor (name) {
+//     this.name = name;
+//   }
 
-  introduceSelf () {
-    console.log(`Hi! I'm ${this.name}`);
-  }
-}
-const youssef = new Person('Youssef');
-youssef.introduceSelf();
+//   introduceSelf () {
+//     console.log(`Hi! I'm ${this.name}`);
+//   }
+// }
+// const youssef = new Person('Youssef');
+// youssef.introduceSelf();
+
+
+
+// function add(x, y){
+//   return x+y;
+// }
+// console.log(add(7, 2));
+
+const add = (x, y) => x + y;
+console.log(add(7, 2));
