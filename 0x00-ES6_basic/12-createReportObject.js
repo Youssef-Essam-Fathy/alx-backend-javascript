e6f5c097d77d28974ee3b8183272038c078ec6f8
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-05-28 03:10:49 +0300

    alx-backend-javascript

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,9 +2,9 @@ import createEmployeesObject from './11-createEmployeesObject';
 
 export default function createReportObject(employeesList) {
   return {
-      allEmployees: {...employeesList},
-      getNumberOfDepartments(employeesList){
-        return Object.keys(employeesList).length;
-      },
+    allEmployees: {...employeesList},
+    getNumberOfDepartments(employeesList){
+      return Object.keys(employeesList).length;
+    },
   };
 }
