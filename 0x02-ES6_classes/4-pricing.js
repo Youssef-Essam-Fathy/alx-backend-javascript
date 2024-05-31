5c6ed8bd78fc8f2d80b537208960afe57dd287af
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-05-31 04:10:53 +0300

    using two methods

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,49 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof currency !== 'object') {
+      throw new TypeError('Currency must be an object of a class Currency');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (typeof newCurrency === 'object') {
+      this._amount = newCurrency;
+    } else {
+      throw new TypeError('Currency must be an object of a class Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  convertPrice(amount, conversionRate) {
+    this._amount = amount;
+    this._conversionRate = conversionRate;
+    return this._amount * this._conversionRate;
+  }
+}
