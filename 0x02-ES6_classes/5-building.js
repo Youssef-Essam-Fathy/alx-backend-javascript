ebbac26b3a37a31888cdbf6df3a83499b757a938
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-05-31 16:35:55 +0300

    using an abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,15 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') throw new TypeError('Sqft must be a number');
-    if (new.target === Building) throw new TypeError('Can\'t construct Building instances directly');
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage')
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  static evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
