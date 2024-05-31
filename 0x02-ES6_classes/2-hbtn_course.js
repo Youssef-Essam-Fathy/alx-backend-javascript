2c64476cc8a864098fa3b0a07530d50ebe2c122e
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-05-31 03:23:36 +0300

    adding a getter and setter to the classroom

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,16 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string' && name.length <= 0) throw new TypeError('Name must be a string');
-    if (typeof length !== 'number') throw new TypeError('Length must be a number');
-    if (!Array.isArray(students)) throw new TypeError('Students must be an array');
-    if (students.some((student) => typeof student !== 'string')) throw new TypeError('Students must be an array of strings');
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
