57d118b800562c46cc54c0c698d726c07c7fe6ed
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-02 01:39:02 +0300

    Debugged the code successfuly

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -13,8 +13,7 @@ export class HolbertonClass {
   }
 }
 
-
-export class StudentHolberton{
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -36,7 +35,6 @@ export class StudentHolberton{
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
