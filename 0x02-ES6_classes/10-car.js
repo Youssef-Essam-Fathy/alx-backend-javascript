ab4daf5bada1b5d24c3d2edd6b64d7486edb9a42
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-02 01:51:47 +0300

    Creating a new class instance from a class method

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,18 +1,11 @@
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  static get [Symbol.species]() {
-    return this;
-  }
-
   cloneCar() {
-    const Species = this.constructor[Symbol.species];
-    return new Species();
+    return new this.constructor();
   }
 }
-
-export default Car;
