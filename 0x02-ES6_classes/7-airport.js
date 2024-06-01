80c67c19ce66264c73fe44f836365d567f5b1a36
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-01 15:01:14 +0300

    Implementing the toString() class method

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,12 +1,18 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
-  get [Symbol.toStringTag]() {
+  get name() {
+    return this._name;
+  }
+
+  get code() {
     return this._code;
   }
-}
 
-export default Airport;
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
