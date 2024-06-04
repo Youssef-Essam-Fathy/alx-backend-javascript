8298d5dfcce06463cb46e1754ca4ea0041d722e7
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-04 14:18:55 +0300

    change the value of a key in the map when value is 1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   for (const [key, value] of map) {
     if (value === 1) map.set(key, 100);
   }
-  if (!(map instanceof Map)) throw new Error('Cannot process');
-
-  return map;
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process')
+  };
 }
