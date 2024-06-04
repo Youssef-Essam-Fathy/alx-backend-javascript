5d111df4fda8c452402d0a18a392c8497b52f630
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-04 08:03:45 +0300

    clean set returning a string separated with -

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,15 @@
 export default function cleanSet(set, startingString) {
-  if (startingString === '') return '';
-
+  if (
+    !set && !(set instanceof Set) && typeof startingString !== 'string' && !startingString
+  ) {
+    return '';
+  }
   const stringArray = [];
 
   for (const string of set) {
-    if (string.startsWith(startingString)) stringArray.push(string.slice(startingString.length));
+    if (string.startsWith(startingString)) {
+      stringArray.push(string.slice(startingString.length));
+    }
   }
 
   return stringArray.join('-');
