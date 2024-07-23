ecd830d17e618680a57f1da815743163388d1737
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-07-24 02:57:45 +0300

    reading from a csv file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+
+function countStudents(path) {
+  try {
+  const data = fs.readFileSync(path, 'utf-8')
+  const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+  const students = lines.slice(1);
+
+  console.log(`Number of students: ${students.length}`);
+
+  const fields = {}
+
+  students.forEach((student) => {
+    const [firstname, , , field] = student.split(',');
+    if (!fields[field]) {
+      fields[field] = {count: 0 , field: []}
+    }
+    fields[field].count += 1;
+    fields[field].field.push(firstname);
+  });
+
+  Object.entries(fields).forEach(([field, data]) => {
+    console.log(`Number of students in ${field}: ${data.count}. List: ${fields[field].field.join(', ')}`);
+  })
+
+    } catch(error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
