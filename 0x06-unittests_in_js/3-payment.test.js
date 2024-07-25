3cc61ceeef5dbd1b2e38940204d22d9a30d82193
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-07-26 02:11:45 +0300

    Done

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,17 +1,16 @@
-const sinon = require('sinon');
-const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./3-payment');
-const Utils = require('./utils');
+const {describe, it} = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require("./utils");
+const assert = require("assert");
 
-describe('sendPaymentRequestToApi', function() {
-  it('should use Utils.calculateNumber with correct arguments', function() {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+describe("sendPaymentRequestToApi", function() {
+    it("check that Utils.calculateNumber was called once", function() {
+	const spy = sinon.spy(Utils, "calculateNumber");
 
-    sendPaymentRequestToApi(100, 20);
+	sendPaymentRequestToApi(50, 24.52);
 
-    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    expect(calculateNumberSpy.returnValues[0]).to.equal(120);
-
-    calculateNumberSpy.restore();
-  });
+	assert(spy.calledOnce);
+	spy.restore();
+    });
 });
